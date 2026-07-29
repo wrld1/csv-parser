020a1f7470c08ff93ecdccb2b86c28f1c3aba15c
wrld1 <kir260302@gmail.com>
2026-07-29 19:18:34 +0300

    Init commit

---
+++ schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS records (
+  id    bigserial PRIMARY KEY,
+  value text NOT NULL
+);
---
+++ src/import.ts
@@ -0,0 +1,3 @@
+import { createReadStream } from "node:fs";
+import { parse } from "csv-parse";
+import pg from "pg";
